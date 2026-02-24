9c67d3bb6df86270e2421bbfd484ec0770f8553f
Radu Edward-Andrei <raduedwardandrei@gmail.com>
2026-02-24 23:47:41 +0200

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Radu Edward-Andrei on 24.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
